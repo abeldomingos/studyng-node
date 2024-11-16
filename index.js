2e66fb10e46bf890961e35c1c66097d10d0d686c
Abel Kamulingui <70151385+abeldomingos@users.noreply.github.com>
2024-11-16 15:42:21 +0100

    Express installation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const app = express()
+const app = express();
 
 app.listen(8081, function() {
-    console.log("O Servidor esta rodando na url http://localhosty:8081")
+    console.log("O Servidor esta rodando na url http://localhosty:8081");
     
 })
