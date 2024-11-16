22a6e483671f0e72db18a714cff6cff2daa14ae5
Abel Kamulingui <70151385+abeldomingos@users.noreply.github.com>
2024-11-16 16:08:56 +0100

    Commit Routes and Parameters

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # studyng node
  new change
+ 
+ # Processo de aprendizado do Node js
+ - O que é Node js
+ - Como instalar o node js
+ - Modulos
+ - Protocolo HTTP
+ - Instalando o Express
+ - Rotas 
+ - Parâmetros
+
--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 
+
+//Criar a nossa 1º Rota
+app.get("/", function(req, res) {
+    res.send("Seja Bem Vindo ao nosso app");
+    
+})
+
+//Outras Rota em Sobre
+app.get("/sobre", function(req, res) {
+    res.send("Sobre a empresa")
+    
+})
+
+// Outra Rota de Service
+app.get("/service", function(req, res){
+    res.send("Nossos services estão abertos")
+})
+
+
+
+
 app.listen(8081, function() {
     console.log("O Servidor esta rodando na url http://localhosty:8081");
     
